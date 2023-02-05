a98492f3dbdec91326732e3d6c35a9cb8def2d9d
39sho <g.39sho@gmail.com>
2023-02-05 11:29:31 +0900

    replace parseFloat() with Number()

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const observer = new MutationObserver(records => {
   document.querySelectorAll("[data-argument-type~='number'], [data-argument-type~='text']")
     .forEach(e => {
       const background = e.querySelector(".blocklyBlockBackground");
-      if (isNaN(parseFloat(e.querySelector("text").textContent))) {
+      if (isNaN(Number(e.querySelector("text").textContent))) {
         background.style.fill = "lightpink";
       } else {
         background.style.fill = "lightgreen";
